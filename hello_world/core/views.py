81b148f22c0dd08bf32fc550b91ac4b4000b0315
Foneyyy <143535190+Foneyyy@users.noreply.github.com>
2023-10-28 14:41:27 +0000

    Template example

--- hello_world/core/views.py
+++ hello_world/core/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+
 def index(request):
     context = {
         "title": "Django example",
     }
     return render(request, "index.html", context)
+
+
+def example_index(request):
+    context = {}
+    return render(request, "example_w10/index.html", context=context)
+
