385a57b9c991f29d97c1d25ec7cb6efcd8f3fd97
Teerachet Piriew <97576398+PIPEPIR@users.noreply.github.com>
2023-11-08 07:02:20 +0000

    edit

--- hello_world/core/views.py
+++ hello_world/core/views.py
@@ -61,11 +61,14 @@ def import_data_csv(request):
     return JsonResponse({"success_indexs":sucesss, "error_indexs": errors})
 
 def model(request):
+    # Assuming you have imported necessary libraries and defined FootballMatches
+
     matches = FootballMatches.objects.all().values()
     df = pd.DataFrame(matches)
 
     label_encoders = {}
-    categorical_features = ['home_team', 'away_team']  
+    categorical_features = ['home_team', 'away_team']
+    
     for feature in categorical_features:
         le = LabelEncoder()
         df[feature] = le.fit_transform(df[feature])
@@ -73,9 +76,9 @@ def model(request):
 
     df = df.drop(columns=['date'])
 
-    X = df.drop(columns=['result'])  
-    X['home_goals'] = df['home_goals']  
-    X['away_goals'] = df['away_goals']  
+    X = df.drop(columns=['result', 'id'])
+    X['home_goals'] = df['home_goals']
+    X['away_goals'] = df['away_goals']
     y = df['result']
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
@@ -83,14 +86,24 @@ def model(request):
     clf = RandomForestClassifier(n_estimators=100, random_state=42)
     clf.fit(X_train, y_train)
 
+    # Create a new row of data for the match you want to predict
+    new_match_data = {
+        'home_team': request.POST.get('home_team', 'Chelsea'),
+        'away_team': request.POST.get('away_team', 'Fulham'),
+        'home_goals': 0,  # You can specify the actual values for home and away goals
+        'away_goals': 0,
+    }
 
-    outcome_probabilities = clf.predict_proba(X_test)
+    # Transform the new_match_data using label encoders
+    for feature in categorical_features:
+        new_match_data[feature] = label_encoders[feature].transform([new_match_data[feature]])[0]
+
+    # Convert the new_match_data to a DataFrame and use it for prediction
+    new_match_df = pd.DataFrame([new_match_data])
+    outcome_probabilities = clf.predict_proba(new_match_df)
 
     response_data = {
-        'label_encoders': {feature: le.classes_.tolist() for feature, le in label_encoders.items()},
-        'sample_outcome_probabilities': outcome_probabilities.tolist(),
-
+        'outcome_probabilities': outcome_probabilities.tolist(),
     }
 
     return JsonResponse(response_data)
-
